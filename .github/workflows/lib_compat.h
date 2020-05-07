93da112040aa551727c68af3df3ca6a2cd60a876
Sam McCall <sam.mccall@gmail.com>
2020-05-07 12:55:33 +0200

    Link against older, more-compatible versiosn of glibc symbols.
    They're binary-compatible, the old versions are more widely available
    and the new versions are more highly optimized. We prefer the former.

---
+++ .github/workflows/lib_compat.h
@@ -0,0 +1,18 @@
+// This header forces the use of a particular version of glibc symbols.
+// This can make the resulting binary portable to systems with older glibcs.
+//
+// It must be included in each TU, with CFLAGS="-include glibc_compat.h" etc.
+//
+// We only list symbols known to be used. This is paired with a test enforcing
+// the max glibc symbol version. If the test fails, the list should be extended.
+// Find the old alternatives version to target with e.g.:
+//   objdump -T /lib/x86_64-linux-gnu/lib{c,m,pthread,rt}.so.* | grep "\bexpf\b"
+
+#define FORCE_SYMBOL_VERSION(sym, version) \
+  __asm__(".symver " #sym "," #sym "@" #version)
+
+FORCE_SYMBOL_VERSION(expf, GLIBC_2.2.5);
+FORCE_SYMBOL_VERSION(log, GLIBC_2.2.5);
+FORCE_SYMBOL_VERSION(pow, GLIBC_2.2.5);
+
+#undef FORCE_SYMBOL_VERSION
