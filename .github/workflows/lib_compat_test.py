73d33422ce4b6c1c69a214ae824951c7cbb7aff3
Sam McCall <sam.mccall@gmail.com>
2020-05-07 14:05:14 +0200

    Address comments (except main, so far)

--- .github/workflows/lib_compat_test.py
+++ .github/workflows/lib_compat_test.py
@@ -19,21 +19,18 @@ args = parser.parse_args()
 # Parses GLIBC_2.3 into ("GLIBC", [2,3])
 # Parses GLIBC into ("GLIBC", None)
 def parse_version(version):
-  parts = version.split('_', 1)
+  parts = version.rsplit('_', 1)
   if len(parts) == 1:
     return (version, None)
-  return (parts[0], [int(p) for p in parts[1].split('.')])
+  try:
+    return (parts[0], [int(p) for p in parts[1].split('.')])
+  except ValueError:
+    return (version, None)
 
 lib_versions = dict([parse_version(v) for v in args.lib])
 
-# Determines whether an optionally-versioned symbol is acceptable.
-def accept_symbol(sym):
-  if sym in args.sym:
-    return True
-  split = sym.split('@', 1)
-  return (split[0] in args.sym) or (len(split) == 2 and accept_lib(split[1]))
-
 # Determines whether all symbols with version 'lib' are acceptable.
+# A versioned library is name_x.y.z by convention.
 def accept_lib(lib):
   (lib, ver) = parse_version(lib)
   if not lib in lib_versions: # Non-whitelisted library.
@@ -43,6 +40,14 @@ def accept_lib(lib):
   if ver is None: # Lib has non-numeric version, library restricts version.
     return False
   return ver <= lib_versions[lib]
+
+# Determines whether an optionally-versioned symbol is acceptable.
+# A versioned symbol is symbol@version as output by nm.
+def accept_symbol(sym):
+  if sym in args.sym:
+    return True
+  split = sym.split('@', 1)
+  return (split[0] in args.sym) or (len(split) == 2 and accept_lib(split[1]))
 
 # Run nm to find the undefined symbols, and check whether each is acceptable.
 nm = subprocess.run(['nm', '-uD', '--with-symbol-version', args.binary],
